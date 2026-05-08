408f8204fa7c1a1fb07e7b26d50a7bb413c353f6
jeonsungwon9012 <155720418+jeonsungwon9012@users.noreply.github.com>
2026-05-08 11:21:15 +0900

    [Passed] 세 실수의 반올림 (164ms, 11MB)

--- trail0/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
+        double temp;
         for(int i=0;i<3;i++){
-            double temp = sc.nextDouble();
+            temp = sc.nextDouble();
             System.out.printf("%.3f\n",temp);
         }
     }
