6384651cdc7f2c89adc73120be8ba7d6fbc59013
jeonsungwon9012 <155720418+jeonsungwon9012@users.noreply.github.com>
2026-05-08 15:13:10 +0900

    [Passed] 합과 평균의 차 (125ms, 10MB)

---
+++ trail0/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt(), b=sc.nextInt(), c=sc.nextInt();
+        int sum = a + b + c;
+        System.out.println(sum);
+        System.out.println(sum/3);
+        System.out.println(sum - (sum/3));
+        
+
+    }
+}
