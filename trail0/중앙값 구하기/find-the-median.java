2d5cbd4a7d8c8242957a6849a4e714cea7fad59b
jeonsungwon9012 <155720418+jeonsungwon9012@users.noreply.github.com>
2026-05-09 16:10:09 +0900

    [Passed] 중앙값 구하기 (127ms, 10MB)

--- trail0/중앙값 구하기/find-the-median.java
+++ trail0/중앙값 구하기/find-the-median.java
@@ -8,13 +8,15 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-
+        // b<a<c 또는 c<a<b 
         if ((a >= b && a <= c) || (a >= c && a <= b)) {
             System.out.println(a);
         }
+        // a<b<c 또는 c<b<a
         else if ((b >= a && b <= c) || (b >= c && b <= a)) {
             System.out.println(b);
         }
+        // a<c<b 또는 b<c<a
         else {
             System.out.println(c);
         }
