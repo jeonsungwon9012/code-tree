f8b83d9bfd968a7a56238a303ccbf2abe1a5a40d
jeonsungwon9012 <155720418+jeonsungwon9012@users.noreply.github.com>
2026-05-09 16:39:47 +0900

    [Passed] 일의 자리 배열 (133ms, 10MB)

--- trail0/일의 자리 배열/array-with-units.java
+++ trail0/일의 자리 배열/array-with-units.java
@@ -1,16 +1,28 @@
 import java.util.*;
+
 public class Main {
+
+    static final int SIZE = 10;
+
     public static void main(String[] args) {
-        // Please write your code here.
+
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
+
+        int[] arr = new int[SIZE];
+
         arr[0] = sc.nextInt();
         arr[1] = sc.nextInt();
-        for(int i= 2; i<10;i++){
-            arr[i] = (arr[i-1]+arr[i-2]) % 10;
+
+        for (int i = 2; i < SIZE; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
         }
-        for(int e : arr){
-            System.out.print(e+" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : arr) {
+            sb.append(num).append(" ");
         }
+
+        System.out.println(sb);
     }
 }
