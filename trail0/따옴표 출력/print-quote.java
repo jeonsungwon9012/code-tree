e3e89d676c722861844306865612d274657d71c2
jeonsungwon9012 <155720418+jeonsungwon9012@users.noreply.github.com>
2026-05-09 16:47:50 +0900

    [Passed] 따옴표 출력 (86ms, 9MB)

---
+++ trail0/따옴표 출력/print-quote.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        StringBuilder sb = new StringBuilder();
+        sb.append("He says \"It\'s a really simple sentence\".");
+        System.out.println(sb);
+
+    }
+}
